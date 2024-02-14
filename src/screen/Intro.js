454d4720ab0acfb65e091cf990dcde5fb7ff6879
mayankjonwal02 <jonwal.1@iitj.ac.in>
2024-02-14 06:24:32 +0530

    updated resume

--- src/screen/Intro.js
+++ src/screen/Intro.js
@@ -11,7 +11,7 @@ import Contact from "../component/Contact";
 
 export default function Intro() {
 
-  const driveLink = 'https://drive.google.com/file/d/1aKZH0EfdnVYPchlNObhK2FPbTwHau8i6/view?usp=drive_link';
+  const driveLink = 'https://drive.google.com/file/d/1OaOqyNhuJKBrKG_l5zkhFmD2cuLgi_sa/view?usp=sharing';
   return (
     <div>
       <Particlebackground />
